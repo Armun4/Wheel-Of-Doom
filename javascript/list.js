2ed866331a2905adc80a13693670556dc277d7bb
a-xo <eiekseau@gmail.com>
2021-04-22 11:43:49 +0200

    fixed grid layout, code formattong, hrader CSS formatting, synta error

--- javascript/list.js
+++ javascript/list.js
@@ -1,146 +1,144 @@
 let coderList = [
-    {
-        name: "Abdulha",
-        img: "Photos/cryptopunks/cp15.png",
-        id:0,
-    },
-    
-    {
-        name:"Leah",
-        img:"Photos/cryptopunks/cp26.png",
-        id:1,
-    },
+  {
+    name: "Abdulha",
+    img: "Photos/cryptopunks/cp15.png",
+    id: 0,
+  },
 
-    {
-        name:"Thierno",
-        img:"Photos/cryptopunks/cp01.png",
-        id:2,
-    },
-    {
-        name:"Armando",
-        img:"Photos/cryptopunks/cp27.png",
-        id:3,
-    },
-    {
-        name:"Anya",
-        img:"Photos/cryptopunks/cp12.png",
-        id:4,
-    },
-    {
-        name:"Alexandra",
-        img:"Photos/cryptopunks/cp14.png",
-        id:5,
-    },
-    {
-        name:"Bertha",
-        img:"Photos/cryptopunks/cp11.png",
-        id:6,
-    },
-    {
-        name:"Franco",
-        img:"Photos/cryptopunks/cp16.png",
-        id:7,
-    },
-    {
-        name:"Martin",
-        img:"Photos/cryptopunks/cp17.png",
-        id:8,
-    },
-    {
-        name:"Salomon",
-        img:"Photos/cryptopunks/cp13.png",
-        id:9,
-    },
-    {
-        name:"Amr",
-        img:"Photos/cryptopunks/cp25.png",
-        id:10,
-    },
-    {
-        name:"Andrea C",
-        img:"Photos/cryptopunks/cp19.png",
-        id:11,
-    },
-    {
-        name:"Andrea E",
-        img:"Photos/cryptopunks/cp23.png",
-        id:12,
-    },
-    {
-        name:"Jael",
-        img:"Photos/cryptopunks/cp14.png",
-        id:13,
-    },
-    {
-        name:"Jose",
-        img:"Photos/cryptopunks/cp20.png",
-        id:14,
-    },
-    {
-        name:"Sergio",
-        img:"Photos/cryptopunks/cp08.png",
-        id:15,
-    },
-    {
-        name:"Cris",
-        img:"Photos/cryptopunks/cp02.png",
-        id:16,
-    },
-    {
-        name:"David H",
-        img:"Photos/cryptopunks/cp30.png",
-        id:16,
-    },
-    {
-        name:"David S",
-        img:"Photos/cryptopunks/cp04.png",
-        id:18,
-    },
-    {
-        name:"Diego G",
-        img:"Photos/cryptopunks/cp29.png",
-        id:19,
-    },
-    {
-        name:"Snzhana",
-        img:"Photos/cryptopunks/cp09.png",
-        id:20,
-    },
-    {
-        name:"Andriu",
-        img:"Photos/cryptopunks/cp21.png",
-        id:21,
-    },
-    {
-        name:"Diego C",
-        img:"Photos/cryptopunks/cp07.png",
-        id:22,
-    },
-     {
-        name:"Sergi",
-        img:"Photos/cryptopunks/cp18.png",
-        id:23,
-    },{
-        name:"Joan",
-        img:"Photos/cryptopunks/cp28.png",
-        id:24,
-    },
-    {
-        name:"Andres",
-        img:"Photos/cryptopunks/cp10.png",
-        id:25,
-    },
-    {
-        name:"Gabi B",
-        img:"Photos/cryptopunks/cp24.png",
-        id:26,
-    },
-    {
-        name:"Gabi P",
-        img:"Photos/cryptopunks/cp06.png",
-        id:27,
-    },
+  {
+    name: "Leah",
+    img: "Photos/cryptopunks/cp12.png",
+    id: 1,
+  },
 
-]
-
-console.log(coderList.length)
+  {
+    name: "Thierno",
+    img: "Photos/cryptopunks/cp01.png",
+    id: 2,
+  },
+  {
+    name: "Armando",
+    img: "Photos/cryptopunks/cp27.png",
+    id: 3,
+  },
+  {
+    name: "Anya",
+    img: "Photos/cryptopunks/cp32.png",
+    id: 4,
+  },
+  {
+    name: "Alex",
+    img: "Photos/cryptopunks/cp26.png",
+    id: 5,
+  },
+  {
+    name: "Bertha",
+    img: "Photos/cryptopunks/cp11.png",
+    id: 6,
+  },
+  {
+    name: "Franco",
+    img: "Photos/cryptopunks/cp16.png",
+    id: 7,
+  },
+  {
+    name: "Martin",
+    img: "Photos/cryptopunks/cp17.png",
+    id: 8,
+  },
+  {
+    name: "Salomon",
+    img: "Photos/cryptopunks/cp13.png",
+    id: 9,
+  },
+  {
+    name: "Amr",
+    img: "Photos/cryptopunks/cp25.png",
+    id: 10,
+  },
+  {
+    name: "Andrea_C",
+    img: "Photos/cryptopunks/cp31.png",
+    id: 11,
+  },
+  {
+    name: "Andrea_E",
+    img: "Photos/cryptopunks/cp23.png",
+    id: 12,
+  },
+  {
+    name: "Jael",
+    img: "Photos/cryptopunks/cp14.png",
+    id: 13,
+  },
+  {
+    name: "Jose_Migel",
+    img: "Photos/cryptopunks/cp20.png",
+    id: 14,
+  },
+  {
+    name: "Sergio",
+    img: "Photos/cryptopunks/cp08.png",
+    id: 15,
+  },
+  {
+    name: "Cris",
+    img: "Photos/cryptopunks/cp02.png",
+    id: 16,
+  },
+  {
+    name: "David_H",
+    img: "Photos/cryptopunks/cp30.png",
+    id: 16,
+  },
+  {
+    name: "David_S",
+    img: "Photos/cryptopunks/cp04.png",
+    id: 18,
+  },
+  {
+    name: "Diego_G",
+    img: "Photos/cryptopunks/cp29.png",
+    id: 19,
+  },
+  {
+    name: "Snezhana",
+    img: "Photos/cryptopunks/cp19.png",
+    id: 20,
+  },
+  {
+    name: "Andriu",
+    img: "Photos/cryptopunks/cp21.png",
+    id: 21,
+  },
+  {
+    name: "Diego_C",
+    img: "Photos/cryptopunks/cp07.png",
+    id: 22,
+  },
+  {
+    name: "Sergi",
+    img: "Photos/cryptopunks/cp18.png",
+    id: 23,
+  },
+  {
+    name: "Joan",
+    img: "Photos/cryptopunks/cp28.png",
+    id: 24,
+  },
+  {
+    name: "Andres",
+    img: "Photos/cryptopunks/cp10.png",
+    id: 25,
+  },
+  {
+    name: "Gabi_B",
+    img: "Photos/cryptopunks/cp24.png",
+    id: 26,
+  },
+  {
+    name: "Gabi_P",
+    img: "Photos/cryptopunks/cp06.png",
+    id: 27,
+  },
+];
