5322efda17405a9d09d9b2c33891468bdb683121
Abdulha Idrees <abdulhaidrees@pop-os.localdomain>
2021-04-23 11:33:33 +0200

    Creste Player

--- javascript/botonadd.js
+++ javascript/botonadd.js
@@ -1,20 +1,8 @@
-/*
-let addbtn = document.getElementById("mybutton")
-addbtn.onclick = newplayer
-
 function newplayer() {
-    let inputvalue = document.getElementById("coderslist").value
-    colors.push ({name:inputvalue})
-    addList()
-    
+    let inputvalue = document.querySelector(".input").value
+    let imgsearch = document.querySelector(".photo")
+    coderList.push({name:inputvalue,img: imgsearch.src, id: coderList.length,life:true})
+    exit()
+    createPlayers()
+    slider()
 }
-
-function  addplayer () {
-    let getcolors = document.getElementById("coderslist")
-    getcolors.innerHTML=``
-    for (let i=0; i<colors.length;i++) {
-    getcolors.innerHTML += `<li>${coderList[i].name}</li>`
-      }
-    
-}
-*/
--- javascript/ficha.js
+++ javascript/ficha.js
@@ -5,49 +5,36 @@ function filemodal() {
     newplayer += `
     <div class="container">
     <section   class="product-description">
+        <button onClick="exit()" class="exit">X</button>
         <h3 class="newplayer">New player</h3> 
         <h3 class="name">name</h3> 
         <input type="text" class="input" >
      <div  class="photo_botton">
         <img src="./Photos/cryptopunks/cp03.png" class="photo"> <div class="voting"> 
             <!-- Up button -->
-             <button class="voting__button">
                   <div class="voting__triangle voting__triangle--up"></div> 
-                </button>
                  <!-- Number --> 
-                <div class="voting__number"> 1 </div>
-                 <!-- Down button --> <button class="voting__button"> 
+                <div class="voting__number"> </div>
+                 <!-- Down button --> 
                      <div class="voting__triangle voting__triangle--down">
 
-                     </div> </button>
+                     </div>
 </div>
 </div>
 
 
 
-    <a href="#" class="myButton">Create</a>
+    <a href="#" onClick="newplayer()" class="myButton">Create</a>
 
     </section>
     </div>`
 
     document.getElementById("root").innerHTML = newplayer
 }
+
 plusbutton.addEventListener ("click", filemodal )
-/*
-function newfile() {
-    let listfile = ""
-    let number = coderList.length
-    for (let i = 0; i < number; i++) {
-        listfile += `
-    <div class="coders_display" onclick="filemodal" id="${coderList[i].index}">
-        <img src="${coderList[i].img}" alt="${coderList[i].alt}">
-    </div>
-    `
-    }
-    console.log(document.querySelector(".coders_display"))
-    document.querySelector(".coders_display").innerHTML = listfile
+
+
+function exit() {
+    document.querySelector("#root").innerHTML = ""
 }
-newfile()
-
-
-*/
