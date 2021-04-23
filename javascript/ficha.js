b7fc3c668d375ad32c8a059aac1493613b2dd9cd
Armun4 <82039854+Armun4@users.noreply.github.com>
2021-04-23 12:34:28 +0200

    paco

--- javascript/ficha.js
+++ javascript/ficha.js
@@ -4,30 +4,36 @@ function filemodal() {
     let newplayer = ""
     newplayer += `
     <div class="container">
-    <section   class="product-description">
-        <button onClick="exit()" class="exit">X</button>
-        <h3 class="newplayer">New player</h3> 
-        <h3 class="name">name</h3> 
-        <input type="text" class="input" >
-     <div  class="photo_botton">
-        <img src="./Photos/cryptopunks/cp03.png" class="photo"> <div class="voting"> 
-            <!-- Up button -->
-                  <div class="voting__triangle voting__triangle--up"></div> 
-                 <!-- Number --> 
-                <div class="voting__number"> </div>
-                 <!-- Down button --> 
-                     <div class="voting__triangle voting__triangle--down">
-
-                     </div>
-</div>
-</div>
-
-
-
-    <a href="#" onClick="newplayer()" class="myButton">Create</a>
-
+    <section class="product-description">
+      <button onClick="exit()" class="exit">X</button>
+      <h3 class="newplayer">New player</h3>
+      <h3 class="name">name</h3>
+      <input type="text" class="input" />
+      <div class="photo_botton">
+        <img src="./Photos/cryptopunks/cp20.png" id="14" class="photo" />
+        <div class="voting">
+          <!-- Up button -->
+          <div
+            class="voting__triangle voting__triangle--up"
+            id="up"
+            onClick="arrowSelect(this)"
+          ></div>
+          <!-- Number -->
+          <div class="voting__number"></div>
+          <!-- Down button -->
+          <div
+            class="voting__triangle voting__triangle--down"
+            id="down"
+            onClick="arrowSelect(this)"
+          ></div>
+        </div>
+      </div>
+  
+      <a href="#" onClick="newplayer()" class="myButton">Create</a>
     </section>
-    </div>`
+  </div>
+  `
+  
 
     document.getElementById("root").innerHTML = newplayer
 }
@@ -37,4 +43,19 @@ plusbutton.addEventListener ("click", filemodal )
 
 function exit() {
     document.querySelector("#root").innerHTML = ""
+}
+
+function arrowSelect(select){
+    let photoselect = document.querySelector(".photo")
+    if (select.id === "up"){
+      photoselect.src= coderList[photoselect.id ++].img
+      photoselect.id= photoselect.id ++
+    }
+    else if (select.id === "down"){
+
+        photoselect.src=coderList[photoselect.id --].img
+        photoselect.id= photoselect.id --
+    }
+    
+
 }
