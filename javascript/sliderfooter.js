435743419a296c5edc3d01ea9ed795909130b521
Abdulha Idrees <abdulhaidrees@pop-os.localdomain>
2021-04-23 09:55:40 +0200

    player dead

--- javascript/sliderfooter.js
+++ javascript/sliderfooter.js
@@ -1,7 +1,8 @@
 function createPlayers() {
     let coder = ""
     for (let i = 0; i < coderList.length; i++){
-        coder += `
+        if (coderList[i].life === true) {
+            coder += `
         <li class="coder-token">
             <figure class="coder-avatar">
                 <img src="${coderList[i].img}" alt="" />
@@ -9,6 +10,17 @@ function createPlayers() {
             <figcaption class="coder-name">${coderList[i].name}</figcaption>
         </li>
         `
+        }
+        if (coderList[i].life === false) {
+            coder += `
+        <li class="coder-token">
+            <figure class="coder-avatar">
+                <img src="./Photos/calavera.png" alt="" />
+            </figure>
+            <figcaption class="coder-name">${coderList[i].name}</figcaption>
+        </li>
+        `
+        }
     }
     document.querySelector(".coders-list").innerHTML = coder
 }
@@ -17,13 +29,16 @@ createPlayers()
 function slider() {
     let player = ""
     for (let i = 0; i < coderList.length; i++){
-    player += `
+        if (coderList[i].life === true){
+            player += `
         <div id="${i}">
             <h2>${coderList[i].name}</h2>
             <img src="${coderList[i].img}"/>
         </div>
         `
     }
+        }
+    
     document.querySelector(".slider").innerHTML = player
 }
 slider()
@@ -31,22 +46,30 @@ slider()
 const botonGo = document.querySelector(".btn1")
 
 function go() {
-    let slider = document.querySelector(".slider")
-    let player = slider.offsetWidth / coderList.length
-    let numberRandom = Math.floor((Math.random() * coderList.length - 1)+1)
-    console.log(numberRandom)
+    slider()
+    let sliders = document.querySelector(".slider")
+    let playersLife = document.querySelectorAll(".slider div")
+    let player = sliders.offsetWidth / playersLife.length
+    let numberRandom = Math.floor((Math.random() * playersLife.length - 1)+1)
+    let dead = playersLife[numberRandom].id
+    console.log(dead)
     let meNewPosition = - player * numberRandom
-    slider.style.left = (meNewPosition + player) + "px" 
-    newDead(numberRandom)
+    sliders.style.left = (meNewPosition + player) + "px" 
+    coderList[dead].life = false
+    createPlayers()
 }
-function newDead(numb) {
-    let slider = document.querySelector(".slider")
-    let player = slider.offsetWidth / coderList.length
-    console.log()
-    // slider anchura
-    // como de ancho 
-    // coger el numero random
-    // operacion matematica
 
-}
 botonGo.addEventListener("click", go)
+
+const botonReset = document.querySelector(".btn2")
+
+function reset() {  
+    for (let i = 0; i < coderList.length; i++) {
+        coderList[i].life = true
+        document.querySelector(".slider").style.left = -62 + "px"
+        slider()
+        createPlayers()
+    }
+}
+
+botonReset.addEventListener("click", reset)
