1738cb3860aa51fdfc8fd4abbbb8216cf970d6e3
Armun4 <82039854+Armun4@users.noreply.github.com>
2021-04-27 14:36:07 +0200

    sounds

--- javascript/gameFlow.js
+++ javascript/gameFlow.js
@@ -8,8 +8,8 @@ function startRoulette() {
     createPlayers()
 }
 function roulettesound() {
-    let audio = document.getElementById("roulettesound")
-    audio.play()
+    let audioR = document.getElementById("roulettesound")
+    audioR.play()
 }
 function rouletteSpin() {
     let sliders = document.querySelector(".slider")
@@ -22,14 +22,24 @@ function rouletteSpin() {
 function getRandomNumber(array) {
     return Math.floor((Math.random() * array.length - 1)+1)
 }
+
+function aliveSound( ){
+    let audioA = document.getElementById("aliveSound")
+    audioA.play()
+}
+
+
 function resurection() {  
     for (let i = 0; i < coderList.length; i++) {
         coderList[i].life = true
         document.querySelector(".slider").style.left = -62 + "px"
         upDateRoulette()
         createPlayers()
+        aliveSound()
     }
 }
+
+
 
 botonGo.addEventListener("click", startRoulette)
 botonReset.addEventListener("click", resurection)
