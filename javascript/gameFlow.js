456db0a3cec1b63839869c14d3715d7ef48a982d
PPIICCAA <abdulhaidrees@gmail.com>
2021-04-30 14:02:03 +0200

    borde casi resuelto

--- javascript/gameFlow.js
+++ javascript/gameFlow.js
@@ -2,7 +2,6 @@ const botonGo = document.querySelector(".btn1")
 const botonReset = document.querySelector(".btn2")
 
 function startRoulette() {
-    // timeLimit()
     roulettesound()
     upDateRoulette()
     rouletteSpin()
@@ -11,14 +10,7 @@ function startRoulette() {
     }, 4000)
     
 }
-function timeLimit() {
-    let period = 10000;       // 5 minutes
-    let ahora = new Date();
-    let milisegundos = ahora.getMilliseconds();
-    for( let tStart = milisegundos;  (milisegundos-tStart) < period;){
-    console.log("hi")
-    }
-}
+
 function roulettesound() {
     let audioR = document.getElementById("roulettesound")
     audioR.currentTime= 0
@@ -30,20 +22,28 @@ function rouletteSpin() {
     let player = sliders.offsetWidth / playersLife.length
     let numberRandom = getRandomNumber(playersLife)
     sliders.style.left = ((- player * numberRandom) + player) + "px" 
-    coderList[playersLife[numberRandom].id].life = false
     bordermove(numberRandom)
+    coderList[playersLife[numberRandom].id].life = false
 }
 async function bordermove(number) {
     for(let i = 0; i  <= number; i++){
         let mydiv = document.querySelectorAll(".coder-token")
         let numberdiv = mydiv[i].id
-            if (coderList[numberdiv].life == true){
-
-            mydiv[i].classList.add("selector") 
+        if (coderList[numberdiv].life == false && i != number) {
+            number ++
+        }   
+         if (coderList[numberdiv].life == true){
+            mydiv[numberdiv].classList.add("selector") 
             setTimeout(() => {
-                mydiv[i].classList.remove("selector")
+                mydiv[numberdiv].classList.remove("selector")
             }, 300)
             await sleep(3000 / number);
+        }
+        if (coderList[numberdiv].life == false && i === number ){
+            mydiv[number].classList.add("selector") 
+            setTimeout(() => {
+                mydiv[number].classList.remove("selector")
+            }, 600)
         }
     }
 }
